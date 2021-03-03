40969f7ea7a3cba414b7c2307205f224c497357b
ghimaja2207 <ghimaja45@gmail.com>
2021-03-03 23:24:13 +0530

    adding files of demo

---
+++ src/main/java/com/sample/demo/Controller.java
@@ -0,0 +1,12 @@
+package com.sample.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/test")
+    public String test(){
+        return "Test Successful";
+    }
+}
